4f0192bb5ed89ac0b03096cffa363ee13734922d
Kirk Kosinski <kirkkosinski@gmail.com>
2025-02-25 19:55:22 -0800

    Run terraform fmt

--- variables.tf
+++ variables.tf
@@ -1,14 +1,14 @@
 variable "zone_name" {
-  type = string
+  type        = string
   description = "Name of the zone."
 }
 
 variable "zone_dns_name" {
-  type = string
+  type        = string
   description = "DNS name of the zone."
 }
 
 variable "dkim" {
-  type = string
+  type        = string
   description = "DKIM record value."
 }
